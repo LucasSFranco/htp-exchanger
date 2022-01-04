bf631e6ae8630fa0738bd2f7272ffdb7cf6523c5
Lucas Surdi Franco <surdifranco@gmail.com>
2022-01-03 23:40:01 -0300

    change lines in selection

--- src/exchange.ts
+++ src/exchange.ts
@@ -1,33 +1,31 @@
 import * as vscode from 'vscode';
 
-function getSelectedText(selection: vscode.Selection, document: vscode.TextDocument): { text: string, range: vscode.Range } {
-    let range = new vscode.Range(selection.start, selection.end);
-
-    return { text: range ? document.getText(range) : undefined, range };
-}
-
 export function exchange() {
     const editor = vscode.window.activeTextEditor;
 
     const { document, selections } = editor;
 
     editor.edit(editBuilder => {
-        const replacementActions = selections.map(selection => {
-            const { text, range } = getSelectedText(selection, document);
+        for (const selection of selections) {
+            const startLine = selection.start.line;
+            const endLine = selection.end.line;
+        
+            for (let line = startLine; line <= endLine; line++) {
+                const range = document.lineAt(line).range;
+                const text = range ? document.getText(range) : undefined;
+
+                if(/^[ \t]*$/.test(text)) continue;
+
+                let replacement; 
             
-            let replacement; 
-            
-            if (/htp.prn\('.*'\);/g.test(text)) {
-                replacement = text.replace(/htp.prn\('(.*)'\);/g, '$1').replace(/''/g, "'");
-            } else {
-                replacement = text.replace(/'/g, "''").replace(/([ \t]*)(.+)/g, "$1htp.prn('$2');");
+                if (/htp.prn\('.*'\);/g.test(text)) {
+                    replacement = text.replace(/htp.prn\('(.*)'\);/g, '$1').replace(/''/g, "'");
+                } else {
+                    replacement = text.replace(/'/g, "''").replace(/([ \t]*)(.+)/g, "$1htp.prn('$2');");
+                }
+
+                editBuilder.replace(range, replacement);
             }
-
-            return { text, range, replacement };
-        });
-
-        replacementActions.forEach(x => {
-            editBuilder.replace(x.range, x.replacement);
-        });
+        }
     });
 }
