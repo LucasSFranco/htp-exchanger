76b02443a7ccf533b2a831606522e4ca46dd784c
Lucas Surdi Franco <surdifranco@gmail.com>
2022-01-12 23:36:49 -0300

    adjust selection position

--- src/exchange.ts
+++ src/exchange.ts
@@ -1,31 +1,60 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
+
+type Replacement = {
+    text: string
+    range: vscode.Range
+    newText: string
+    newRange: vscode.Range
+}
+
+function toSelection(range: vscode.Range): vscode.Selection {
+    return new vscode.Selection(
+        range.start.line, range.start.character,
+        range.end.line, range.end.character
+    )
+}
 
 export function exchange() {
-    const editor = vscode.window.activeTextEditor;
+    const editor = vscode.window.activeTextEditor
 
-    const { document, selections } = editor;
+    const { document, selections } = editor
+
+    let replacements: Replacement[] = []
 
     editor.edit(editBuilder => {
-        for (const selection of selections) {
-            const startLine = selection.start.line;
-            const endLine = selection.end.line;
-        
-            for (let line = startLine; line <= endLine; line++) {
-                const range = document.lineAt(line).range;
-                const text = range ? document.getText(range) : undefined;
+        selections.forEach((s) => {
+            for (let line = s.start.line; line <= s.end.line; line++) {
+                let range = document.lineAt(line).range
+                const text = range ? document.getText(range) : undefined
 
-                if(/^[ \t]*$/.test(text)) continue;
+                if(/^[ \t]*$/.test(text)) continue
 
-                let replacement; 
+                let offset: number
+                let newText: string
             
                 if (/htp.prn\('.*'\);/g.test(text)) {
-                    replacement = text.replace(/htp.prn\('(.*)'\);/g, '$1').replace(/''/g, "'");
+                    offset = (s.start.character >= 9 && s.end.character <= text.length - 3) ? -9 : 0
+                    newText = text.replace(/htp.prn\('(.*)'\);/g, '$1').replace(/''/g, "'")
                 } else {
-                    replacement = text.replace(/'/g, "''").replace(/([ \t]*)(.+)/g, "$1htp.prn('$2');");
+                    offset = 9
+                    newText = text.replace(/'/g, "''").replace(/([ \t]*)(.+)/g, "$1htp.prn('$2');")
                 }
 
-                editBuilder.replace(range, replacement);
+                replacements.push({
+                    text,
+                    range,
+                    newText,
+                    newRange: new vscode.Range(
+                        s.start.line, s.start.character + offset,
+                        s.end.line, s.end.character + offset
+                    )
+                })
+                
             }
-        }
-    });
+        })
+
+        replacements.forEach((r) => { editBuilder.replace(r.range, r.newText) })
+    }).then(() => {
+        editor.selections = replacements.map((r) => toSelection(r.newRange))
+    })
 }
